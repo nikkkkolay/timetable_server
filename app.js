f4ac2754921ab3061d9a4b1e796716c12afc86fc
turboreaktivnayatyaga <ggn.f764@gmail.com>
2024-06-09 23:34:08 +0300

    коннект с бд

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log('hello world');
+import express from 'express';
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.listen(8080, () => console.log('Старт сервера'));
+
+const connection = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('Есть контакт!');
+});
+
+connection.query('SELECT * FROM `groups`', function (error, results, fields) {
+    if (error) throw error;
+    console.log('The solution is: ', results[0]);
+});
+
+connection.end();
