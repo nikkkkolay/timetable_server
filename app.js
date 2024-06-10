975f443a6aecafae6dc9db9a5674fd513975197a
turboreaktivnayatyaga <ggn.f764@gmail.com>
2024-06-11 00:10:08 +0300

    добавил swagger

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJSDoc from 'swagger-jsdoc';
+
 import {
     getCourses,
     getFaculties,
@@ -13,34 +16,128 @@ import {
 } from './database.js';
 
 const app = express();
+const swaggerOptions = {
+    swaggerDefinition: {
+        info: {
+            title: 'Расписание МАУ',
+            description: 'API Расписание МАУ',
+        },
+        servers: ['http://localhost:8080'],
+    },
+    apis: ['app.js'],
+};
 
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+
+app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+/**
+ * @swagger
+ * /:
+ *  get:
+ *   description: Получить дату обновления расписания
+ *   responses:
+ *     '200':
+ *       description: Новая дата обновления
+ */
 app.get('/', async (req, res) => {
     const update = await getUpdateDate();
     res.send(update[0]);
 });
 
+/**
+ * @swagger
+ * /courses:
+ *  get:
+ *   description: Получить список курсов
+ *   responses:
+ *     '200':
+ *       description: Список курсов
+ */
 app.get('/courses', async (req, res) => {
     const courses = await getCourses();
     res.send(courses[0]);
 });
 
+/**
+ * @swagger
+ * /faculties:
+ *  get:
+ *   description: Получить список институтов
+ *   responses:
+ *     '200':
+ *       description: Список институтов
+ */
 app.get('/faculties', async (req, res) => {
     const faculties = await getFaculties();
     res.send(faculties[0]);
 });
 
+/**
+ * @swagger
+ * /groups/fac_id={fac_id}/course_id={course_id}:
+ *  get:
+ *   description: Получить список доступных групп
+ *   parameters:
+ *    - in: path
+ *      name: fac_id
+ *      required: true
+ *      type: number
+ *      minimum: 1
+ *      description: id института/факультета
+ *    - in: path
+ *      name: course_id
+ *      required: true
+ *      type: number
+ *      description: id курса
+ *   responses:
+ *     '200':
+ *       description: Список доступных групп
+ */
 app.get('/groups/fac_id=:fac_id/course_id=:course_id', async (req, res) => {
     const { fac_id, course_id } = req.params;
     const groups = await getGroups(fac_id, course_id);
     res.send(groups[0]);
 });
 
+/**
+ * @swagger
+ * /schedule-group={group_id}:
+ *  get:
+ *   description: Получить список доступных дат
+ *   parameters:
+ *    - in: path
+ *      name: group_id
+ *      required: true
+ *      type: number
+ *      minimum: 1
+ *      description: id группы
+ *   responses:
+ *     '200':
+ *       description: Список доступных доступных дат
+ */
 app.get('/schedule-group=:group_id', async (req, res) => {
     const { group_id } = req.params;
     const dates = await getAvailableDates(group_id);
     res.send(dates[0]);
 });
 
+/**
+ * @swagger
+ * /schedule/group_id={group_id}:
+ *  get:
+ *   description: Получить расписание на текущую дату
+ *   parameters:
+ *    - in: path
+ *      name: group_id
+ *      required: true
+ *      type: number
+ *      minimum: 1
+ *      description: id группы
+ *   responses:
+ *     '200':
+ *       description: Расписание на текущую дату
+ */
 app.get('/schedule/group_id=:group_id', async (req, res) => {
     const { group_id } = req.params;
     const date = await getCurrentSchedule(group_id);
@@ -51,6 +148,32 @@ app.get('/schedule/group_id=:group_id', async (req, res) => {
     }
 });
 
+/**
+ * @swagger
+ * /schedule/group_id={group_id}/at={start}/to={end}':
+ *  get:
+ *   description: Получить расписание в диапазоне дат
+ *   parameters:
+ *    - in: path
+ *      name: group_id
+ *      required: true
+ *      type: number
+ *      minimum: 1
+ *      description: id группы
+ *    - in: path
+ *      name: start
+ *      required: true
+ *      type: string
+ *      description: YYYY-MM-DD
+ *    - in: path
+ *      name: end
+ *      required: true
+ *      type: string
+ *      description: YYYY-MM-DD
+ *   responses:
+ *     '200':
+ *       description: Расписание в диапазоне дат
+ */
 app.get('/schedule/group_id=:group_id/at=:start/to=:end/', async (req, res) => {
     const { group_id, start, end } = req.params;
 
