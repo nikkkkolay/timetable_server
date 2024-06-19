5651983e6ed31c7deb6ddfff2fa09c3c7deae211
Гагара Николай Федорович <gagaranf@mauniver.ru>
2024-06-19 16:38:51 +0300

    диапазон возможных дать

--- app.js
+++ app.js
@@ -147,7 +147,7 @@ app.get('/groups/:group_name', async (req, res) => {
 app.get('/available-dates/:group_id', async (req, res) => {
     const { group_id } = req.params;
     const dates = await getAvailableDates(group_id);
-    res.send([dates[0][0].pair_date, dates[0][dates[0].length - 1].pair_date]);
+    if (dates) res.send([dates[0][0].pair_date, dates[0][dates[0].length - 1].pair_date]);
 });
 
 /**
